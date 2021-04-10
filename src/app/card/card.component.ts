515a3bdfad93fd4da152032d38cc345a3539fcf4
Giovanni Izzi <giovanni9972@libero.it>
2021-04-10 14:50:30 +0200

    fix style + viewEncapsulating.none

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CardComponent implements OnInit {
 
